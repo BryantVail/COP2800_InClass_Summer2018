034ca2c80708dd0407d539df1910013f283be384
Bryant Vail <Bryanttv@outlook.com>
2018-06-24 23:53:11 -0400

    done

--- pr3/pr3/Main.java
+++ pr3/pr3/Main.java
@@ -1,3 +1,5 @@
+//Bryant Vail
+
 package pr3;
 
 import javafx.application.Application;
@@ -15,7 +17,10 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		TextField txtOutput = new TextField("Selection");
 		
-		Scene sceneMain = new Scene();
+		//stage
+		primaryStage.setTitle("Project 3");
+		
+		
 		VBox vbox = new VBox();
 		RadioButton radio1 = new RadioButton("1");
 		RadioButton radio2 = new RadioButton("2");
@@ -24,7 +29,10 @@ public class Main extends Application {
 		ToggleGroup radioGroup = new ToggleGroup();
 		//set ToggleGroup
 		radio1.setToggleGroup(radioGroup);
+		radio1.setSelected(true);
+		
 		radio2.setToggleGroup(radioGroup);
+		
 		radio3.setToggleGroup(radioGroup);
 		
 		radio1.setOnAction(e ->{
@@ -40,12 +48,20 @@ public class Main extends Application {
 		//label
 		Label lblQuestion = new Label("How many do you select?");
 		
+		vbox.getChildren().add(lblQuestion);
+		vbox.getChildren().add(radio1);
+		vbox.getChildren().add(radio2);
+		vbox.getChildren().add(radio3);
+		vbox.getChildren().add(txtOutput);
 		
+		Scene sceneMain = new Scene(vbox, 300,300);
+		primaryStage.setScene(sceneMain);
+		primaryStage.show();
 	}//end public void start
 	
 	public static void main(String[] args) {
 			
-		
+		Application.launch(args);
 
 	}
 
