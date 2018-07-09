1d62c13a3b7ca08419bf74bb49f42d60091fb1af
Bryant Vail <Bryanttv@outlook.com>
2018-07-08 23:51:50 -0400

    arc completed

--- pr3/pr3/Main.java
+++ pr3/pr3/Main.java
@@ -1,11 +1,17 @@
 package pr3;
+//Bryant Vail
 
+
+import javafx.animation.Animation;
 import javafx.animation.PathTransition;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -19,24 +25,27 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		Pane pane = new Pane();
 		
+		
+		
+		HBox hbox = new HBox();
+		
+		
 		//circle that will be animated
 		Circle orbitCircle = new Circle(75, 50, 20);
 		orbitCircle.setFill(Color.ORANGE);
 		
 		
-		//circle for arc
-		Circle arcCircle = new Circle(125, 100, 50);
-		arcCircle.setFill(Color.WHITE);
-		arcCircle.setStroke(Color.BLACK);
+		//create arc
+		Arc pendulumArc = new Arc(100,200,80,45,45,100);
+		pendulumArc.setStroke(Color.BLACK);
+		pendulumArc.setFill(Color.WHITE);
 		
-		//add circles to the pane
-		pane.getChildren().add(orbitCircle);
-		pane.getChildren().add(arcCircle);
+		
 		
 		//create path transition
 		PathTransition pt = new PathTransition();
 		pt.setDuration(Duration.millis(4000));
-		pt.setPath(arcCircle);
+		pt.setPath(pendulumArc);
 		pt.setNode(orbitCircle);
 		pt.setOrientation(
 				PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT
@@ -45,12 +54,28 @@ public class Main extends Application {
 		pt.setAutoReverse(true);
 		pt.play();
 		
-		arcCircle.setOnMousePressed(e -> {
+		pendulumArc.setOnMousePressed(e -> {
 			pt.pause();
 		});
-		arcCircle.setOnMouseReleased(e -> {
+		pendulumArc.setOnMouseReleased(e -> {
 			pt.play();
 		});
+		
+		Button toggleButton = new Button("Toggle Motion");
+		toggleButton.setOnAction(e -> {
+			if(pt.getStatus() == Animation.Status.RUNNING) {
+				pt.pause();
+			}else {
+				pt.play();
+			}
+		});
+		
+		//add circles to the pane
+		pane.getChildren().add(hbox);
+		pane.getChildren().add(orbitCircle);
+		pane.getChildren().add(pendulumArc);
+		
+		hbox.getChildren().add(toggleButton);
 		
 		//create scene
 		Scene scene = new Scene(pane, 250, 200);
